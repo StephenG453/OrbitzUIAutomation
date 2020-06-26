42440db093fdb5ee68b41cb85a80127ea93ab796
StephenG98 <txsteven@gmail.com>
2020-06-26 09:47:12 -0500

    refactoring - cleanup SearchResultsPage

--- acceptanceTests/src/main/java/webpages/SearchResultsPage.java
+++ acceptanceTests/src/main/java/webpages/SearchResultsPage.java
@@ -18,25 +18,14 @@ public class SearchResultsPage extends WebDriverInitiation {
 
     public void getNumberOfItineraries() {
         WebDriverWait wait = new WebDriverWait(driver, 10);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//ul[@id='flightModuleList']")));
-
-//        WebElement results = driver.findElement(By.xpath("//ul[@id='flightModuleList']"));
-//        WebElement results = driver.findElement(By.xpath("//h3[contains(text(),'Result')]"));
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//ul[@id='flightModuleList']")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("flightModuleList")));
 
         List<WebElement> itineraries = driver.findElements(By.xpath("//h3[contains(text(),'Result')]"));
-//
-//        for (WebElement itin : itineraries) {
-//            System.out.println("itin: " + itin.getText());
-//        }
-
         System.out.println("All itineraries available: " + itineraries.size());
     }
 
     public void pressNonStopButton() {
-//        WebDriverWait wait = new WebDriverWait(driver, 10);
-//        WebElement nonstopButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//fieldset[@id='stops']//div[3]//div[1]//label[1]")));
-//        nonstopButton.click();
-
         int attempts = 0;
         while (attempts < 2) {
             try {
@@ -58,10 +47,6 @@ public class SearchResultsPage extends WebDriverInitiation {
     }
 
     public void selectFirstListedFlight() {
-//        WebDriverWait wait = new WebDriverWait(driver, 10);
-//        WebElement selectButtonForFirstFlight = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//li[1]//div[1]//div[2]//div[2]//div[1]//div[2]//button[1]")));
-//        selectButtonForFirstFlight.click();
-
         int attempts = 0;
         while (attempts < 2) {
             try {
@@ -80,10 +65,6 @@ public class SearchResultsPage extends WebDriverInitiation {
      }
 
      public void pressSelectThisFareButton() {
-//         WebDriverWait wait = new WebDriverWait(driver, 10);
-//         WebElement selectThisFareButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//li[1]//div[2]//div[1]//div[1]//div[1]//div[1]//button[1]")));
-//         selectThisFareButton.click();
-
          int attempts = 0;
          while (attempts < 2) {
              try {
