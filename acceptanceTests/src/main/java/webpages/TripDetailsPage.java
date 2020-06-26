ebae63253b54a9b6b18ea1a2c96bc349b674808a
StephenG98 <txsteven@gmail.com>
2020-06-26 09:48:07 -0500

    refactoring - cleanup TripDetailsPage

--- acceptanceTests/src/main/java/webpages/TripDetailsPage.java
+++ acceptanceTests/src/main/java/webpages/TripDetailsPage.java
@@ -1,16 +1,11 @@
 package webpages;
 
-import org.junit.jupiter.api.Assertions;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class TripDetailsPage extends WebDriverInitiation {
-
-    TripDetailsPage tripDetailsPage;
 
     public TripDetailsPage(WebDriver driver) {
         super(driver);
